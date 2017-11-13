7f6655ce115fd26339dd63f8f281bb238f523301
Will Lisac <will.lisac@servicenow.com>
2017-11-12 17:28:56 -0800

    Initial Commit

---
+++ SnowChat/SnowChat.h
@@ -0,0 +1,19 @@
+//
+//  SnowChat.h
+//  SnowChat
+//
+//  Created by Will Lisac on 11/12/17.
+//  Copyright © 2017 ServiceNow. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SnowChat.
+FOUNDATION_EXPORT double SnowChatVersionNumber;
+
+//! Project version string for SnowChat.
+FOUNDATION_EXPORT const unsigned char SnowChatVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SnowChat/PublicHeader.h>
+
+
