b34e83632c391e7a26c90f6f7d42ff42de1c819d
Michael Borowiec <michael.borowiec@servicenow.com>
2017-11-30 13:51:13 -0800

    EPIC0037008: Fixed warnings

--- Foundation/CGPathUtils.swift
+++ Foundation/CGPathUtils.swift
@@ -12,11 +12,11 @@ func chatBubblePath(forBounds bounds: CGRect, radius: CGFloat = 7, tip: CGFloat 
     let rect = bounds
     let path = CGMutablePath()
     path.move(to: CGPoint(x: rect.minX, y: rect.maxY))
-    path.addArc(center: CGPoint(x: rect.minX, y: rect.maxY-tip), radius: tip, startAngle: 1.5*(.pi), endAngle: 0, clockwise: true)
+    path.addArc(center: CGPoint(x: rect.minX, y: rect.maxY - tip), radius: tip, startAngle: 1.5 * (.pi), endAngle: 0, clockwise: true)
     
-    path.addArc(center: CGPoint(x: rect.minX+radius + tip, y: rect.minY+radius), radius: radius, startAngle: .pi, endAngle: 3.0*(.pi/2.0), clockwise: false)
-    path.addArc(center: CGPoint(x: rect.maxX-radius, y: rect.minY+radius), radius: radius, startAngle: 3.0*(.pi/2.0), endAngle: 0, clockwise: false)
-    path.addArc(center: CGPoint(x: rect.maxX-radius, y: rect.maxY-radius), radius: radius, startAngle: 0, endAngle: .pi/2, clockwise: false)
+    path.addArc(center: CGPoint(x: rect.minX + radius + tip, y: rect.minY + radius), radius: radius, startAngle: .pi, endAngle: 3.0 * (.pi / 2.0), clockwise: false)
+    path.addArc(center: CGPoint(x: rect.maxX - radius, y: rect.minY + radius), radius: radius, startAngle: 3.0 * (.pi / 2.0), endAngle: 0, clockwise: false)
+    path.addArc(center: CGPoint(x: rect.maxX - radius, y: rect.maxY - radius), radius: radius, startAngle: 0, endAngle: .pi / 2, clockwise: false)
     path.addLine(to: CGPoint(x: rect.minX, y: rect.maxY))
     path.closeSubpath()
     return path
