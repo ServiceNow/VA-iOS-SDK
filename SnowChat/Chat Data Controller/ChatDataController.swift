48c4706995583c166c27470b2800f5dafcee2103
Will Lisac <will.lisac@servicenow.com>
2017-12-11 16:16:28 -0800

    STRY4762583: Add Chat Window / Chat Service scaffolding
    - Add `SlackTextViewController` framework
    - Add parent `ChatViewController` with child `ConversationViewController`
    - Create public `ChatService` class that exposes a `ChatViewController`
    - Add `ChatDataController` that will be used to abstract chatterbox to the UI code

---
+++ SnowChat/Chat Data Controller/ChatDataController.swift
@@ -0,0 +1,19 @@
+//
+//  ChatDataController.swift
+//  SnowChat
+//
+//  Created by Will Lisac on 12/11/17.
+//  Copyright © 2017 ServiceNow. All rights reserved.
+//
+
+import Foundation
+
+class ChatDataController {
+    
+    private let chatterbox: Chatterbox
+    
+    init(chatterbox: Chatterbox) {
+        self.chatterbox = chatterbox
+    }
+    
+}
