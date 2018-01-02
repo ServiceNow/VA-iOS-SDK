878f1a4a88fc6dce4a586bf484840482b610e9f8
Michael Borowiec <michael.borowiec@servicenow.com>
2018-01-02 09:43:21 -0800

    STRY4762572: Added image loader protocol

--- Controls/OutputImageControl.swift
+++ Controls/OutputImageControl.swift
@@ -6,6 +6,15 @@
 //  Copyright © 2017 ServiceNow. All rights reserved.
 //
 
+// FIXME: this protocol will have to be improved
+
+// whoever uses OutputImageControl is responsible for conforming and implementing ImageDownloader protocol
+// ImageDownloader was added to make OutputImageControl independent of networking client
+public protocol ImageDownloader: AnyObject {
+    
+    func downloadImage(forURLRequest request: URLRequest, completion: (UIImage?, Error?) -> Void)
+}
+
 class OutputImageControl: ControlProtocol {
     
     var model: ControlViewModel
@@ -14,20 +23,34 @@ class OutputImageControl: ControlProtocol {
     
     weak var delegate: ControlDelegate?
     
+    private var imageURLRequest: URLRequest?
+    
+    weak var imageDownloader: ImageDownloader? {
+        didSet {
+            
+            guard let urlRequest = imageURLRequest else {
+                fatalError("Image url request doesn't exist")
+            }
+            
+            imageDownloader?.downloadImage(forURLRequest: urlRequest, completion: { [weak self] (image, error) in
+                
+                if let error = error {
+                    Logger.default.logError("Error loading image from URL \(error)")
+                    return
+                }
+                
+                (self?.viewController as? OutputImageViewController)?.image = image
+            })
+        }
+    }
+    
     required init(model: ControlViewModel) {
         guard let imageModel = model as? OutputImageViewModel else {
             fatalError("Wrong model class")
         }
         
         self.model = imageModel
-        
-        let bundle = Bundle(for: OutputImageViewController.self)
-        let imageController = OutputImageViewController()
-        guard let image = UIImage(named: "mark.png", in: bundle, compatibleWith: nil) else {
-            fatalError("Couldn't load image")
-        }
-        
-        imageController.image = image
-        self.viewController = imageController
+        self.imageURLRequest = URLRequest(url: imageModel.value)
+        self.viewController = OutputImageViewController()
     }
 }
