15ddb3826ed1e5408796c1af699c6ed4bfdd2280
Michael Borowiec <michael.borowiec@servicenow.com>
2018-01-04 09:06:46 -0800

    STRY4762567: Changed creds to demonightlychatbot

--- SnowChat/Chatterbox/Data/CBData.swift
+++ SnowChat/Chatterbox/Data/CBData.swift
@@ -39,7 +39,7 @@ class CBData {
     }
     
     // swiftlint:disable:next force_unwrapping
-    static var config: ChatBoxConfig = ChatBoxConfig(url: URL(string: "http://localhost:8080")!)
+    static var config: ChatBoxConfig = ChatBoxConfig(url: URL(string: "https://demonightlychatbot.service-now.com")!)
     
     struct ChatBoxConfig {
         var url: URL
