f8db8e43ea34ea3fccdf6a6113f1ee6ed223ab9d
Michael Borowiec <michael.borowiec@servicenow.com>
2018-01-17 08:53:36 -0800

    STRY4980652: Added some unit tests and moved Footer and Header view to a seperate class

---
+++ SnowChatTests/Controls/ControlCacheTests.swift
@@ -0,0 +1,63 @@
+//
+//  ControlCacheTests.swift
+//  SnowChatTests
+//
+//  Created by Michael Borowiec on 1/17/18.
+//  Copyright © 2018 ServiceNow. All rights reserved.
+//
+
+import XCTest
+@testable import SnowChat
+
+class ControlCacheTests: XCTestCase {
+    
+    private var controlCache = ControlCache()
+    
+    override func setUp() {
+        super.setUp()
+    }
+    
+    override func tearDown() {
+        super.tearDown()
+        controlCache.removeAll()
+    }
+    
+    func testReuseTextControl() {
+        let firstTextControlModel = TextControlViewModel(id: "first_text_control", value: "Sample text")
+        let firstTextControl = controlCache.control(forModel: firstTextControlModel)
+        XCTAssert(firstTextControl.model.id == "first_text_control")
+        
+        controlCache.removeControl(forModel: firstTextControlModel)
+        let secondTextControlModel = TextControlViewModel(id: "second_text_control", value: "Another sample text")
+        let secondTextControl = controlCache.control(forModel: secondTextControlModel)
+        
+        // Compare objects' addresses
+        XCTAssert(firstTextControl === secondTextControl)
+        XCTAssert(firstTextControlModel !== secondTextControlModel)
+        XCTAssert(secondTextControl.model.id == "second_text_control")
+        
+        // This should create new control since there nothing to reuse
+        let thirdTextControl = controlCache.control(forModel: firstTextControlModel)
+        XCTAssert(secondTextControl !== thirdTextControl)
+    }
+    
+    func testReuseTextAndBooleanControl() {
+        let firstTextControlModel = TextControlViewModel(id: "first_text_control", value: "Sample text")
+        let booleanControlModel = BooleanControlViewModel(id: "boolean_control", required: true)
+        let booleanControl = controlCache.control(forModel: booleanControlModel)
+        let textControl = controlCache.control(forModel: firstTextControlModel)
+        
+        // Nothing to reuse yet so we should get new TextControl
+        let secondTextControlModel = TextControlViewModel(id: "second_text_control", value: "Sample text")
+        let secondTextControl = controlCache.control(forModel: secondTextControlModel)
+        XCTAssert(secondTextControl !== textControl)
+        
+        // Prepare for reuse boolean control
+        controlCache.removeControl(forModel: booleanControlModel)
+        let thirdTextControlModel = TextControlViewModel(id: "third_text_control", value: "Sample text")
+        let control = controlCache.control(forModel: thirdTextControlModel)
+        
+        // Make sure cache is not returning wrong control type
+        XCTAssert(control !== booleanControl)
+    }
+}
