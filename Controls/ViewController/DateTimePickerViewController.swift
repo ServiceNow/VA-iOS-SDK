1170732961319a67980267e0bdf9bf40922580e3
Michael Borowiec <michael.borowiec@servicenow.com>
2018-02-13 16:09:12 -0800

    STRY4762566: Added displayMode to DateTimePickerViewController

--- Controls/ViewController/DateTimePickerViewController.swift
+++ Controls/ViewController/DateTimePickerViewController.swift
@@ -10,17 +10,75 @@ import UIKit
 
 class DateTimePickerViewController: UIViewController {
     
+    private enum DisplayMode {
+        case time
+        case date
+        case dateTime
+    }
+    
     @IBOutlet private weak var datePicker: UIDatePicker!
     @IBOutlet private(set) weak var doneButton: UIButton!
     @IBOutlet private weak var selectedDateLabel: UILabel!
+    @IBOutlet private weak var dateTitleLabel: UILabel!
     
-    var model: DateTimePickerControlViewModel?
+    var model: DateTimePickerControlViewModel? {
+        didSet {
+            guard let model = model else { return }
+            switch model.type {
+            case .dateTime:
+                displayMode = .dateTime
+            case .time:
+                displayMode = .time
+            case .date:
+                displayMode = .date
+            default:
+                fatalError("Wrong model assigned")
+            }
+        }
+    }
+    
+    private var displayMode: DisplayMode = .dateTime {
+        didSet {
+            updateDateTitleLabel()
+            updatePickerMode()
+        }
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
         datePicker.backgroundColor = UIColor.white
         datePicker.addTarget(self, action: #selector(dateChanged(_:)), for: .valueChanged)
         updateSelectedDateLabelWithDate(datePicker.date)
+        updateDateTitleLabel()
+        updatePickerMode()
+    }
+    
+    private func updateDateTitleLabel() {
+        let title: String
+        switch displayMode {
+        case .dateTime:
+            title = NSLocalizedString("Select date and time", comment: "Title label for dateTime picker")
+        case .time:
+            title = NSLocalizedString("Select time", comment: "Title label for time picker")
+        case .date:
+            title = NSLocalizedString("Select date", comment: "Title label for date picker")
+        }
+        
+        selectedDateLabel.text = title
+    }
+    
+    private func updatePickerMode() {
+        let mode: UIDatePickerMode
+        switch displayMode {
+        case .dateTime:
+            mode = .dateAndTime
+        case .time:
+            mode = .time
+        case .date:
+            mode = .date
+        }
+        
+        datePicker.datePickerMode = mode
     }
     
     @objc private func dateChanged(_ sender: UIDatePicker) {
