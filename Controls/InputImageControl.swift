51854a5c68949f91758d1a5fc406cc6ab3a67863
Michael Borowiec <michael.borowiec@servicenow.com>
2018-02-20 08:01:36 -0800

    STRY4980283: Added proper authorization for camera and photoLibrary

--- Controls/InputImageControl.swift
+++ Controls/InputImageControl.swift
@@ -7,6 +7,7 @@
 //
 
 import MobileCoreServices
+import AVFoundation
 import Photos
 
 class InputImageControl: NSObject, PickerControlProtocol, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
@@ -42,37 +43,68 @@ class InputImageControl: NSObject, PickerControlProtocol, UIImagePickerControlle
     func pickerViewController(_ viewController: PickerViewController, didFinishWithModel model: PickerControlViewModel) {
         guard let item = model.selectedItem else { return }
         
-        // Check if Info.plist has a value for NSPhotoLibraryUsageDescription key. Otherwise the app will crash
-        guard let _ = Bundle.main.infoDictionary?["NSPhotoLibraryUsageDescription"] else {
-            fatalError("Please provide value for the NSPhotoLibraryUsageDescription key in Info.plist")
-        }
-        
-        // TODO: Bubble up an error if user declined access?
-        let autorizationStatus = PHPhotoLibrary.authorizationStatus()
-        guard autorizationStatus == .authorized else {
-            Logger.default.logError("User didn't autorize access to Photo Library!")
-            return
-        }
-        
-        let imagePickerControllerSourceType: UIImagePickerControllerSourceType
         switch item.type {
         case .takePhoto:
-            imagePickerControllerSourceType = .camera
+            authorizeCamera({ [weak self] status in
+                self?.presentImagePickerControllerWithSourceType(.camera)
+            })
         case .photoLibrary:
-            guard UIImagePickerController.isCameraDeviceAvailable(.rear) else {
-                // TODO: FatalError? Alert?
-                return
-            }
-            imagePickerControllerSourceType = .photoLibrary
+            authorizePhoto({ [weak self] status in
+                if status == .authorized {
+                    self?.presentImagePickerControllerWithSourceType(.photoLibrary)
+                }
+            })
         default:
             fatalError("Not supported type")
         }
-        
-        presentImagePickerControllerWithSourceType(imagePickerControllerSourceType)
     }
     
     func pickerViewController(_ viewController: PickerViewController, didSelectItem item: PickerItem, forPickerModel pickerModel: PickerControlViewModel) {
         
+    }
+    
+    private func authorizeCamera(_ handler: @escaping (AVAuthorizationStatus) -> Swift.Void) {
+        guard nil != Bundle.main.infoDictionary?["NSCameraUsageDescription"] else {
+            fatalError("Please provide value for the NSCameraUsageDescription key in Info.plist of your application")
+        }
+        
+        guard UIImagePickerController.isCameraDeviceAvailable(.rear) else {
+            DispatchQueue.main.async {
+                handler(.denied)
+            }
+            
+            return
+        }
+        
+        let authorizationStatus = AVCaptureDevice.authorizationStatus(for: .video)
+        if authorizationStatus == .notDetermined {
+            AVCaptureDevice.requestAccess(for: .video) { granted in
+                handler(granted ? .authorized : .denied)
+            }
+        } else {
+            DispatchQueue.main.async {
+                handler(authorizationStatus)
+            }
+        }
+    }
+    
+    private func authorizePhoto(_ handler: @escaping (PHAuthorizationStatus) -> Swift.Void) {
+        
+        // Check if Info.plist has a value for NSPhotoLibraryUsageDescription key. Otherwise the app will crash
+        guard nil != Bundle.main.infoDictionary?["NSPhotoLibraryUsageDescription"] else {
+            fatalError("Please provide value for the NSPhotoLibraryUsageDescription key in Info.plist of your application")
+        }
+        
+        let authorizationStatus = PHPhotoLibrary.authorizationStatus()
+        if authorizationStatus == .notDetermined {
+            PHPhotoLibrary.requestAuthorization({ status in
+                handler(status)
+            })
+        } else {
+            DispatchQueue.main.async {
+                handler(authorizationStatus)
+            }
+        }
     }
     
     // MARK: - UIImagePickerController
