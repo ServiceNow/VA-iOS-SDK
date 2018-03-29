2aeb1afada63773e21fe192eefb6aef01cd6aca1
marc.attinasi <marc.attinasi@servicenow.com>
2018-03-29 13:07:21 -0700

    STRY000: Added test for no-prefix, and also for malformed hex value (too less than 6 digits)

--- SnowChatTests/Util/UIColorHexValueTests.swift
+++ SnowChatTests/Util/UIColorHexValueTests.swift
@@ -13,8 +13,12 @@ class TestHexColors: XCTestCase {
     
     func testNormalHex() {
         let hex = "#FF00FF"
+        let hexNoPrefix = "FF00FF"
         
         let color = UIColor(hexValue: hex)
+        let colorNoPrefix = UIColor(hexValue: hexNoPrefix)
+        
+        XCTAssertEqual(color, colorNoPrefix)
         
         var red: CGFloat = 0.0
         var green: CGFloat = 0.0
@@ -36,4 +40,19 @@ class TestHexColors: XCTestCase {
         color.getRed(&red, green: &green, blue: &blue, alpha: &alpha)
         XCTAssert(red == 1.0 && green == 0 && blue == 1.0 && alpha == 1.0)
     }
+    
+    func testMalformedHex() {
+        let hex = "#F0F0"
+        
+        let color = UIColor(hexValue: hex)
+        
+        var red: CGFloat = 0.0
+        var green: CGFloat = 0.0
+        var blue: CGFloat = 0.0
+        var alpha: CGFloat = 0.0
+        color.getRed(&red, green: &green, blue: &blue, alpha: &alpha)
+        // make sure it is something
+        XCTAssert(red + green + blue > 1.0 && alpha == 1.0)
+    }
+    
 }
