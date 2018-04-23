ee3185802c9f35580c183987b425e037c321246a
marc.attinasi <marc.attinasi@servicenow.com>
2018-04-23 16:42:57 -0700

    STRY5453857: If we get back 0 records from history, remeber that we are at the beginnng so we do not try to fetch again.

--- SnowChat/Chat Window/ConversationViewController.swift
+++ SnowChat/Chat Window/ConversationViewController.swift
@@ -38,7 +38,9 @@ class ConversationViewController: SLKTextViewController, ViewDataChangeListener,
     
     private var canFetchOlderMessages = false
     private var timeLastHistoryFetch: Date = Date()
+    private var atBeginningOfHistory = false
     private var isLoading = false
+    
     private var defaultMessageHeight: CGFloat?
     private var maxMessageHeight: CGFloat?
     private var tableFooterView: PagingTableFooterView?
@@ -347,8 +349,9 @@ extension ConversationViewController {
     
     func fetchOlderMessagesIfPossible() {
         guard canFetchOlderMessages,
+            !atBeginningOfHistory,
             Date().timeIntervalSince(timeLastHistoryFetch) > 1.0 else {
-                Logger.default.logDebug("Skipping fetch of older messages - last one was \(Date().timeIntervalSince(timeLastHistoryFetch)) ago")
+                Logger.default.logDebug("Skipping fetch of older messages")
                 return
         }
             
@@ -357,11 +360,12 @@ extension ConversationViewController {
         dataController.fetchOlderMessages { [weak self] count in
             guard let strongSelf = self else { return }
             
-            if count > 0 {
-                // TODO: need to provide indices for the updated rows...
-                strongSelf.tableView.reloadData()
-            }
+            strongSelf.atBeginningOfHistory = (count == 0)
             
+            guard count > 0 else { return }
+            
+            // TODO: need to provide indices for the updated rows...
+            strongSelf.tableView.reloadData()
             strongSelf.canFetchOlderMessages = true
             strongSelf.timeLastHistoryFetch = Date()
         }
