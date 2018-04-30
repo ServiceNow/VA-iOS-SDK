00ca724d4bb633c12ed115636f3610a1061e1541
Michael Borowiec <michael.borowiec@servicenow.com>
2018-04-30 11:21:31 -0700

    STRY4979685: Updated log level

--- Example/SnowKangaroo/Root UI/HomeViewController.swift
+++ Example/SnowKangaroo/Root UI/HomeViewController.swift
@@ -60,7 +60,7 @@ class HomeViewController: UIViewController, ChatServiceDelegate {
         ChatService.loggers().forEach { (logger) in
             switch logger.category {
             case "Chatterbox", "DataController":
-                logger.logLevel = .debug
+                logger.logLevel = .error
             default:
                 logger.logLevel = .fatal
             }
